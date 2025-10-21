35c8eb9fa13542742693aef1c59706381aee5992
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-10-21 10:19:09 +0500

    changes

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { loginUser } from '../controllers/authController.js';
-import { addGame, deleteGame, getAllGames } from '../controllers/gameController.js';
+import { addGame, deleteGame, editGame, getAllGames } from '../controllers/gameController.js';
 
 const router = express.Router();
 
 router.post('/login' , loginUser);
 router.post('/add', addGame);
-router.put('/update/:id', addGame);
+router.put('/update/:id', editGame);
 router.delete('/delete/:id', deleteGame);
 router.get('/get-all' ,  getAllGames)
 
